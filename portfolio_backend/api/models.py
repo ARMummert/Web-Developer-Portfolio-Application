869e20802752c5094000bd8e9f0f94d0f490fef1
ARMummert <armummert4@gmail.com>
2024-01-17 10:20:44 -0800

    added backend updates

--- portfolio_backend/api/models.py
+++ portfolio_backend/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    github = models.URLField(blank=True)
+    demo = models.URLField(blank=True)
+    image = models.ImageField(upload_to='portfolio/images/')
+    # image = models.ImageField(upload_to='portfolio/images/', default='portfolio/images/placeholder.png')
+    show = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
